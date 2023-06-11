d7fc55d7162ccfcece05d36a4b344dab94956cc4
Christian Froehlecke <chris.froh@live.com>
2023-06-12 00:27:55 +0200

    Add speed average

--- lib/sensor/speed.py
+++ lib/sensor/speed.py
@@ -21,6 +21,7 @@ class AngularSpeedSensor(SpeedSensor):
         self.stage_diameter = stage_diameter
         self.last_angle_recording: float | None = None
         self.last_angle: Angle | None = None
+        self.avg_speed: list[float] = []
 
     def measure_speed(self) -> float | None:
         # Initialize values if they are not set yet
@@ -36,7 +37,7 @@ class AngularSpeedSensor(SpeedSensor):
         dt = sensor_recording - self.last_angle_recording
         if dt > 0:
             da = self.last_angle.delta(sensor_angle)
-            if da > 1:
+            if da > 0.25:
                 s = math.radians(da) * self.stage_diameter / 2
                 speed = s / dt
             else:
@@ -45,6 +46,8 @@ class AngularSpeedSensor(SpeedSensor):
             # Update values
             self.last_angle_recording = self.angle_sensor.last_angle_recording
             self.last_angle = self.angle_sensor.last_angle.round()
-            return speed
+            self.avg_speed.append(speed)
+            self.avg_speed = self.avg_speed[-5:]
+            return sum(self.avg_speed) / len(self.avg_speed)
         else:
             return None
