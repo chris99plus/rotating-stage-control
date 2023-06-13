7e5851ed0fada5fba984d35776efdabbf98d9ee2
Christian Froehlecke <chris.froh@live.com>
2023-06-13 13:12:51 +0200

    Fix speed time bug

--- lib/sensor/speed.py
+++ lib/sensor/speed.py
@@ -19,8 +19,9 @@ class AngularSpeedSensor(SpeedSensor):
     def __init__(self, angle_sensor: RotationSensor, stage_diameter: float) -> None:
         self.angle_sensor = angle_sensor
         self.stage_diameter = stage_diameter
-        self.last_angle_recording: float | None = None
         self.last_angle: Angle | None = None
+        self.last_angle_recording: float | None = None
+        self.last_angle_recording_avg: list[float] = []
         self.last_angle_avg: list[Angle] = []
 
     def measure_speed(self) -> float | None:
@@ -29,12 +30,14 @@ class AngularSpeedSensor(SpeedSensor):
             if self.angle_sensor.last_angle is not None:
                 self.last_angle_avg.append(self.angle_sensor.last_angle)
                 self.last_angle = self.angle_sensor.last_angle
+                self.last_angle_recording_avg.append(self.angle_sensor.last_angle_recording)
                 self.last_angle_recording = self.angle_sensor.last_angle_recording
             return None
         
-        assert self.last_angle is not None and self.last_angle_recording is not None
-        sensor_recording = self.angle_sensor.last_angle_recording
+        assert self.last_angle is not None and self.last_angle_recording is not None   
+        self.last_angle_recording_avg.append(self.angle_sensor.last_angle_recording)
         self.last_angle_avg.append(self.angle_sensor.last_angle)
+        sensor_recording = sum(self.last_angle_recording_avg) / len(self.last_angle_recording_avg)
         sensor_angle = angle_avg(self.last_angle_avg)
         dt = sensor_recording - self.last_angle_recording
         if dt > 0:
@@ -43,9 +46,10 @@ class AngularSpeedSensor(SpeedSensor):
             speed = s / dt
 
             # Update values
-            self.last_angle_recording = self.angle_sensor.last_angle_recording
+            self.last_angle_recording_avg = self.last_angle_recording_avg[-10:]
             self.last_angle_avg = self.last_angle_avg[-10:]
             self.last_angle = sensor_angle
+            self.last_angle_recording = sensor_recording
             return speed
         else:
             return None
